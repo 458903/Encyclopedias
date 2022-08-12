568d3fd6dc9a0b6f10722790539314a285964e5b
fanfan <1078189582@qq.com>
2022-08-12 23:47:46 +0800

    1.创建初始项目

---
+++ src/main/java/com/encyclical/mYencycloeadia/MYencycloeadiaApplication.java
@@ -0,0 +1,13 @@
+package com.encyclical.mYencycloeadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MYencycloeadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MYencycloeadiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encyclical/mYencycloeadia/MYencycloeadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encyclical.mYencycloeadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MYencycloeadiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
