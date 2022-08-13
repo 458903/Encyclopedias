ea95b754a4fea813bef3c9ebd5f3245107e3171a
fanfan <1078189582@qq.com>
2022-08-13 11:31:54 +0800

    2.修改启动文案

--- src/main/java/com/encyclical/mYencycloeadia/MYencycloeadiaApplication.java
+++ src/main/java/com/encyclical/mYencycloeadia/MYencycloeadiaApplication.java
@@ -1,13 +1,22 @@
 package com.encyclical.mYencycloeadia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class MYencycloeadiaApplication {
-
+			private static final Logger LOG=LoggerFactory.getLogger(MYencycloeadiaApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(MYencycloeadiaApplication.class, args);
+		SpringApplication app = new SpringApplication(MYencycloeadiaApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+
+		LOG.info("启动成功!!!");
+		LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+
 	}
 
 }
